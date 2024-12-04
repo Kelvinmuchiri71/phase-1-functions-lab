4e104ac1d61086873b6f08fdad501bde78c7a474
Kelvin Muchiri <muchirikelv71@gmail.com>
2024-12-04 15:33:37 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (numnberOfTheBlock) {
+    if (numnberOfTheBlock > 42) {
+        return numnberOfTheBlock - 42;
+    } else {
+        return 42 - numnberOfTheBlock;
+    }
+    }
+    function distanceFromHqInFeet (numnberOfTheBlock) {
+        return distanceFromHqInBlocks(numnberOfTheBlock) * 264;
+    }
+    function distanceTravelledInFeet (startingPoint, destinationPoint) {
+    if (startingPoint < destinationPoint) {
+        return (destinationPoint - startingPoint) * 264;
+    } else {
+        return (startingPoint - destinationPoint) * 264;
+    }
+    }
+    function calculatesFarePrice (startingPoint, destinationPoint) {
+    const distance = distanceTravelledInFeet(startingPoint, destinationPoint);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+    }
